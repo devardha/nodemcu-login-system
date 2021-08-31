7d1be31cf971389b168c5bacf9dd974bfc132170
dev-ardha <ardhaydhtm@gmail.com>
2021-09-01 05:28:43 +0700

    Add rfid reader

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,8 +1,13 @@
 #include <ESP8266WiFi.h>
+#include <MFRC522.h>
 #include <WebSocketsClient.h>
 #include <SPI.h>
 
+#define RST_PIN 5
+#define SDA_PIN 4
+
 WebSocketsClient webSocket;
+MFRC522 mfrc522(SDA_PIN, RST_PIN);
 
 const char* SSID = "";
 const char* PASSWORD = ""; 
@@ -22,6 +27,8 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("IP address : ");
   Serial.println(WiFi.localIP());
+  
+  delay(1000);
 
   webSocket.begin("192.168.1.6", 8000, "/ws");
 
@@ -30,12 +37,44 @@ void setup() {
   webSocket.setReconnectInterval(5000);
   
   webSocket.enableHeartbeat(15000, 3000, 2);
+  
+   mfrc522.PCD_Init();
+  Serial.println("mfrc522 ready");
 }
 
 void loop() {
   webSocket.loop();
+  
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+    
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
 
-  webSocket.send("Hello, wordl!");
+  String content= "";
+  byte letter;
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++){
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  
+  Serial.println();
+
+  content.toUpperCase();
+  
+  if (content.substring(1) == "04 5C 71 BA 8F 61 80"){
+    Serial.println("Authorized access");
+    webSocket.sendTXT("RFID_SUCCESS");
+  } else  {
+    Serial.println("Access denied");
+    webSocket.sendTXT("RFID_FAILED");
+  } 
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
